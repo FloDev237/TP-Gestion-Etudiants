5cf3b67d404119de95b204e199d22a0ee405f544
manu245-sne <manuellesonie@gmail.com>
2025-12-22 17:59:03 +0100

    test push manuelle

---
+++ etudiant.c
@@ -0,0 +1,28 @@
+#include "etudiant.h"
+#include <stdio.h>
+#include <string.h>
+
+// Afficher le menu
+void afficherMenu() {
+    printf("\n.............................................\n");
+    printf(".       MENU DE GESTION DES ETUDIANTS       .\n");
+    printf(".............................................\n");
+    printf(".  1.  Enregistrer un nouvel etudiant       .\n");
+    printf(".  2.  Modifier les informations            .\n");
+    printf(".  3.  Rechercher par matricule             .\n");
+    printf(".  4.  Supprimer un etudiant                .\n");
+    printf(".  5.  Trier par ordre alphabetique         .\n");
+    printf(".  6.  Rechercher par dichotomique          .\n");
+    printf(".  7.  Calculer l'age d'un etudiant         .\n");
+    printf(".  8.  Trier par filiere                    .\n");
+    printf(".  9.  Afficher la liste des etudiants      .\n");
+    printf(".  0.  Quitter l'application                .\n");
+    printf(".............................................\n");
+}
+// Enregistrer un nouvel étudiant
+// Afficher un étudiant
+// Afficher tous les étudiants
+// Modifier un étudiant (avec pointeurs comme demandé)
+// Rechercher par matricule
+// Calculer l'âge
+// Les autres fonctions (à implémenter par ton équipe)
